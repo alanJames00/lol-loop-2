035d392ce04e15104fe7e26b56a8deece6a5e3c5
Alan James <alanjames6282@gmail.com>
2023-08-03 13:40:31 +0000

    fix directory structure to standard

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Joke from "./Joke";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Joke Generator Using React and Joke API</h1>
+      <Joke />
+    </div>
+  );
+}
+
+export default App;
